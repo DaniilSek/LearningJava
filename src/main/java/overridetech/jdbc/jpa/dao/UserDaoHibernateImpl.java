02b267460c8b1c02da4ae1f1383cf5294778dec1
Даниил <nomad1996@yandex.ru>
2025-03-24 01:01:35 +0500

    Hibernate update

--- src/main/java/overridetech/jdbc/jpa/dao/UserDaoHibernateImpl.java
+++ src/main/java/overridetech/jdbc/jpa/dao/UserDaoHibernateImpl.java
@@ -3,10 +3,17 @@ package overridetech.jdbc.jpa.dao;
 import overridetech.jdbc.jpa.model.User;
 
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private SessionFactory sessionFactory;
+
+    public UserDaoHibernateImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
 
@@ -17,26 +24,62 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DROP TABLE IF EXISTS Users");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.load(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery("FROM Users", User.class);
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Query query = session.createQuery("DELETE FROM Users");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/overridetech/jdbc/jpa/util/Util.java
+++ src/main/java/overridetech/jdbc/jpa/util/Util.java
@@ -1,5 +1,42 @@
 package overridetech.jdbc.jpa.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class Util {
+
     // реализуйте настройку соеденения с БД
+    private static SessionFactory sessionFactory;
+
+    private static void setupSessionFactory() {
+        // Создаем службу регистрации Hibernate
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySetting("hibernate.connection.driver_class", "org.postgresql.Driver") // класс драйвера JDBC
+                .applySetting("hibernate.connection.url", "jdbc:postgresql://localhost:5432/database_name") // URL подключения к базе данных
+                .applySetting("hibernate.connection.username", "username")
+                .applySetting("hibernate.connection.password", "password")
+                .applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect") // диалект Hibernate, соответствующий используемой базе данных
+                .applySetting("hibernate.show_sql", true) // настройка для включения/выключения логирования SQL-запросов
+                .applySetting("hibernate.hbm2ddl.auto", "update") // настройка для автоматического создания и обновления схем базы данных
+                .build();
+
+        try {
+            // Создаем фабрику сессий
+            sessionFactory = new MetadataSources(registry)
+                    .addAnnotatedClass(overridetech.jdbc.jpa.model.User.class) // Указываем классы, которые будут управляться Hibernate
+                    .buildMetadata()
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            // Destroy службы регистрации при возникновении ошибки
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
 }
