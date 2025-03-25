939dbe2920ef95e60431ce3f8ef869427b92e415
Daniil Sekletcov <nomad1996@yandex.ru>
2025-03-25 11:22:09 +0500

    Update Util.java

--- src/main/java/overridetech/jdbc/jpa/util/Util.java
+++ src/main/java/overridetech/jdbc/jpa/util/Util.java
@@ -1,42 +1,53 @@
 package overridetech.jdbc.jpa.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+
 public class Util {
 
     // реализуйте настройку соеденения с БД
-    private static SessionFactory sessionFactory;
+    private static final String DB_URL = "jdbc:sqlite::memory:";
 
-    private static void setupSessionFactory() {
-        // Создаем службу регистрации Hibernate
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .applySetting("hibernate.connection.driver_class", "org.postgresql.Driver") // класс драйвера JDBC
-                .applySetting("hibernate.connection.url", "jdbc:postgresql://localhost:5432/database_name") // URL подключения к базе данных
-                .applySetting("hibernate.connection.username", "username")
-                .applySetting("hibernate.connection.password", "password")
-                .applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect") // диалект Hibernate, соответствующий используемой базе данных
-                .applySetting("hibernate.show_sql", true) // настройка для включения/выключения логирования SQL-запросов
-                .applySetting("hibernate.hbm2ddl.auto", "update") // настройка для автоматического создания и обновления схем базы данных
-                .build();
-
+    /**
+     * Метод для создания соединения с базой данных
+     */
+    public static Connection getConnection() {
         try {
-            // Создаем фабрику сессий
-            sessionFactory = new MetadataSources(registry)
-                    .addAnnotatedClass(overridetech.jdbc.jpa.model.User.class) // Указываем классы, которые будут управляться Hibernate
-                    .buildMetadata()
-                    .buildSessionFactory();
-        } catch (Exception e) {
-            // Destroy службы регистрации при возникновении ошибки
-            StandardServiceRegistryBuilder.destroy(registry);
+            return DriverManager.getConnection(DB_URL);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+    public static SessionFactory setupSessionFactory() {
+        // Создаем StandardServiceRegistry для настройки параметров Hibernate
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySetting("hibernate.connection.driver_class", "org.sqlite.JDBC") // класс драйвера JDBC
+                .applySetting("hibernate.connection.url", DB_URL) // URL подключения к базе данных
+                .applySetting("hibernate.dialect", "org.hibernate.dialect.SQLiteDialect") // диалект Hibernate, соответствующий используемой базе данных
+                .applySetting("hibernate.show_sql", true) // настройка для включения/выключения логирования SQL-запросов
+                .applySetting("hibernate.hbm2ddl.auto", "none")
+                //.applySetting("hibernate.hbm2ddl.auto", "update") // настройка для автоматического создания и обновления схем базы данных
+                .build();
 
+        try {
+            // Создаем SessionFactory
+            SessionFactory sessionFactory = new MetadataSources(registry)
+                    .addAnnotatedClass(overridetech.jdbc.jpa.model.User.class) // Указываем классы, которые будут управляться Hibernate
+                    .buildMetadata()
+                    .buildSessionFactory();
+            return sessionFactory;
+        } catch (Exception e) {
+            // Утилизация StandardServiceRegistry при возникновении ошибки
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException(e);
+        }
+    }
 }
