95f28657bc5a8446f0473758a61bf495c52fa9c1
Daniil Sekletcov <nomad1996@yandex.ru>
2025-03-26 15:08:14 +0500

    Update Cat.java

--- SPRING_HELLOWORLD/src/main/java/Cat.java
+++ SPRING_HELLOWORLD/src/main/java/Cat.java
@@ -1,2 +1,38 @@
 public class Cat {
+    private String name;
+    private String breed;
+    private Byte age;
+
+    public Cat() {
+    }
+
+    public Cat(String name, String breed, Byte age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
 }
