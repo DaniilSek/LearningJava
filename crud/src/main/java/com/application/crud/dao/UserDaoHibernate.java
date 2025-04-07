7d63899ab340fe40c1356f76da6fe242deb87943
Daniil Sekletcov <nomad1996@yandex.ru>
2025-04-07 18:34:09 +0500

    Update UserDaoHibernate.java

--- crud/src/main/java/com/application/crud/dao/UserDaoHibernate.java
+++ crud/src/main/java/com/application/crud/dao/UserDaoHibernate.java
@@ -1,9 +1,1 @@
-package com.application.crud.repositories;
-import com.application.crud.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
